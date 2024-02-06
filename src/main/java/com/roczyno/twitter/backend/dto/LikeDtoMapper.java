8fdfb0ce5751ab77892c1938cbc66137350e3f33
roczyno <adiabaroczyno9@gmail.com>
2024-02-06 08:32:31 +0000

    completed the like dto

--- src/main/java/com/roczyno/twitter/backend/dto/LikeDtoMapper.java
+++ src/main/java/com/roczyno/twitter/backend/dto/LikeDtoMapper.java
@@ -3,17 +3,46 @@ package com.roczyno.twitter.backend.dto;
 import com.roczyno.twitter.backend.model.Like;
 import com.roczyno.twitter.backend.model.User;
 
-public class LikeDtoMapper {
-    public static LikeDto toLikeDto(Like like, User reqUser){
-        UserDto user =UserDtoMapper.toUserDto(like.getUser());
-        UserDto reqUserDto= UserDtoMapper.toUserDto(reqUser);
-        TweetDto tweet= TweetDtoMapper.toTweetDto(like.getTweet(),reqUser);
+import java.util.ArrayList;
+import java.util.List;
 
-        LikeDto likeDto= new LikeDto();
+public class LikeDtoMapper {
+    public static LikeDto toLikeDto(Like like, User reqUser) {
+        // Convert the user associated with the like to a UserDto
+        UserDto user = UserDtoMapper.toUserDto(like.getUser());
+
+        // Convert the requesting user (reqUser) to a UserDto
+        UserDto reqUserDto = UserDtoMapper.toUserDto(reqUser);
+
+        // Convert the tweet associated with the like to a TweetDto
+        TweetDto tweet = TweetDtoMapper.toTweetDto(like.getTweet(), reqUser);
+
+        // Create a new LikeDto object and populate its properties
+        LikeDto likeDto = new LikeDto();
         likeDto.setId(like.getId());
         likeDto.setTweet(tweet);
         likeDto.setUser(user);
+
+        // Return the populated LikeDto object
         return likeDto;
+    }
+
+    public static List<LikeDto> toLikeDtos(List<Like> likes,User reqUser){
+        List<LikeDto> likeDtos= new ArrayList<>();
+
+        for (Like like:likes){
+
+            UserDto user = UserDtoMapper.toUserDto(like.getUser());
+
+            TweetDto tweet = TweetDtoMapper.toTweetDto(like.getTweet(), reqUser);
+
+            LikeDto likeDto = new LikeDto();
+            likeDto.setId(like.getId());
+            likeDto.setTweet(tweet);
+            likeDto.setUser(user);
+            likeDtos.add(likeDto);
+        }
+        return likeDtos;
 
     }
 }
